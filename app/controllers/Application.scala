200612cba5dbad33471473632973b1cf8ae94a21
mplis <michael.plis@huffingtonpost.com>
2015-09-05 16:42:54 -0400

    initial commit

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -13,23 +13,7 @@ object Application extends Controller {
     Ok(views.html.index(null))
   }
 
-  def db = Action {
-    var out = ""
-    val conn = DB.getConnection()
-    try {
-      val stmt = conn.createStatement
-
-      stmt.executeUpdate("CREATE TABLE IF NOT EXISTS ticks (tick timestamp)")
-      stmt.executeUpdate("INSERT INTO ticks VALUES (now())")
-
-      val rs = stmt.executeQuery("SELECT tick FROM ticks")
-
-      while (rs.next) {
-        out += "Read from DB: " + rs.getTimestamp("tick") + "\n"
-      }
-    } finally {
-      conn.close()
-    }
-    Ok(out)
+  def admin = Action {
+    Ok(views.html.index(null))
   }
 }
--- build.sbt
+++ build.sbt
@@ -1,4 +1,4 @@
-name := """play-getting-started"""
+name := """rdfl-playoffs"""
 
 version := "1.0-SNAPSHOT"
 
