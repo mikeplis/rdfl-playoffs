e777f7c24e43bba84edc4a2ee20748938f73abbb
mplis <michael.plis@huffingtonpost.com>
2015-09-07 09:18:00 -0400

    update admin form

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -2,18 +2,39 @@ package controllers
 
 import play.api._
 import play.api.mvc._
-import play.api.cache.Cache
+import play.api.data._
+import play.api.data.Forms._
+import play.api.Logger
 import play.api.Play.current
+import play.api.i18n.Messages.Implicits._
 
-import play.api.db._
-
-object Application extends Controller {
+class Application extends Controller {
 
   def index = Action {
-    Ok(views.html.index(null))
+    Ok(views.html.index())
   }
 
   def admin = Action {
-    Ok(views.html.index(null))
+    Ok(views.html.admin(adminForm))
   }
+
+  def adminPost = Action { implicit request =>
+    adminForm.bindFromRequest.fold(
+      formWithErrors => {
+        BadRequest(views.html.admin(formWithErrors))
+      },
+      adminData => {
+        Redirect(routes.Application.index())
+      }
+    )
+  }
+
+  val adminForm = Form(
+    mapping(
+      "teams" -> nonEmptyText,
+      "advancers" -> number(min = 0, max = 100)
+    )(AdminData.apply)(AdminData.unapply)
+  )
 }
+
+case class AdminData(teamIds: String, advancerCount: Int)
