a97793653f5477e359f1e600ab6d9766c8c38e33
mplis <michael.plis@huffingtonpost.com>
2015-09-08 14:46:06 -0400

    replace list of id's with dynamically generated checkboxes on admin form

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -8,6 +8,9 @@ import play.api.Logger
 import play.api.Play.current
 import play.api.i18n.Messages.Implicits._
 
+import play.api.libs.json._
+import scala.io.Source
+
 class Application extends Controller {
 
   def index = Action {
@@ -15,18 +18,36 @@ class Application extends Controller {
   }
 
   def admin = Action {
-    Ok(views.html.admin(adminForm))
+    getFranchiseList.fold(InternalServerError("error getting list of franchises")) { franchises =>
+      Ok(views.html.admin(adminForm, franchises))
+    }
   }
 
   def adminPost = Action { implicit request =>
     adminForm.bindFromRequest.fold(
       formWithErrors => {
-        BadRequest(views.html.admin(formWithErrors))
+        val franchiseList = getFranchiseList.getOrElse(List())
+        BadRequest(views.html.admin(formWithErrors, franchiseList))
       },
       adminData => {
         Redirect(routes.Application.index())
       }
     )
+  }
+
+  def getFranchiseList: Option[List[Franchise]] = {
+    val league = {
+      // TODO: read this from config file
+      val url = "http://football26.myfantasyleague.com/2015/export?TYPE=league&L=34348&W=&JSON=1"
+      val source = Source.fromURL(url).mkString
+      Json.parse(source)
+    }
+    val maybeFranchises = (league \\ "franchise").headOption.map(_.as[List[JsObject]])
+    maybeFranchises.map(_.map { franchiseJson =>
+      val name = (franchiseJson \ "name").as[String]
+      val id = (franchiseJson \ "id").as[String]
+      Franchise(name, id)
+    })
   }
 
   def test = Action {
@@ -35,10 +56,11 @@ class Application extends Controller {
 
   val adminForm = Form(
     mapping(
-      "teams" -> nonEmptyText,
-      "advancers" -> number(min = 0, max = 100)
+      "advancers" -> number(min = 0, max = 100),
+      "teamId" -> list(nonEmptyText)
     )(AdminData.apply)(AdminData.unapply)
   )
 }
 
-case class AdminData(teamIds: String, advancerCount: Int)
+case class AdminData(advancerCount: Int, teamIds: List[String])
+case class Franchise(name: String, id: String)
